1c3d94f42636f5df051bb1c6308857ed1dd651b4
TishaNaturralle <138395666+TishaNaturralle@users.noreply.github.com>
2024-02-04 18:05:55 +0200

    added bootstrap and footer

--- src/App.js
+++ src/App.js
@@ -15,7 +15,20 @@ function App() {
        <Bridal/>
      
       </header>
+      <footer>
+        This project was coded by {" "}
+        <a href="https://www.ticha.io/" target="_blank">
+          Ticha Maenga
+        </a>{" "}
+        and is {" "} 
+        <a href="https://github.com/tishanaturralle/my-dg-react-app"
+        target="_blank"> open sourced</a>
+        
+
+      </footer>
     </div>
+    
+
   );
 }
 
